72812c0cec2444fc53c6dda6393374782eeb4aa9
Y-oHr-N <kon.y.ohr.n@gmail.com>
2019-10-30 13:04:18 +0900

    Add test_reproducibility

--- tests/test_sklearn.py
+++ tests/test_sklearn.py
@@ -97,3 +97,20 @@ def test_feature_importances(n_jobs: int) -> None:
     clf.fit(X, y)
 
     assert isinstance(clf.feature_importances_, np.ndarray)
+
+
+@pytest.mark.parametrize('n_jobs', [-1, 1])
+def test_reproducibility(n_jobs: int) -> None:
+    X, y = load_breast_cancer(return_X_y=True)
+
+    clf = OGBMClassifier(n_jobs=n_jobs, random_state=0)
+
+    clf.fit(X, y)
+
+    probas = clf.predict_proba(X)
+
+    clf = OGBMClassifier(n_jobs=n_jobs, random_state=0)
+
+    clf.fit(X, y)
+
+    assert np.array_equal(probas, clf.predict_proba(X))
