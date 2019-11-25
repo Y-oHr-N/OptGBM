7161278b14630fa338cf3709681de9a60ae4b986
Y-oHr-N <kon.y.ohr.n@gmail.com>
2019-11-25 11:54:03 +0900

    Update transform_batch

--- examples/bike-sharing-demand/config.py
+++ examples/bike-sharing-demand/config.py
@@ -10,9 +10,11 @@ def transform_batch(X):
     """User-defined proprocessing."""
     s = X.index.to_series()
 
+    X['unixtime'] = 1e-09 * s.astype('int64')
+
     attrs = [
-        'year',
-        'weekofyear',
+        # 'year',
+        # 'weekofyear',
         'dayofyear',
         'quarter',
         'month',
@@ -24,7 +26,25 @@ def transform_batch(X):
     ]
 
     for attr in attrs:
-        X[attr] = getattr(s.dt, attr)
+        if attr == 'dayofyear':
+            period = np.where(s.dt.is_leap_year, 366.0, 365.0)
+        elif attr == 'quarter':
+            period = 4.0
+        elif attr == 'month':
+            period = 12.0
+        elif attr == 'day':
+            period = s.dt.daysinmonth
+        elif attr == 'weekday':
+            period = 7.0
+        elif attr == 'hour':
+            period = 24.0
+        elif attr in ['minute', 'second']:
+            period = 60.0
+
+        theta = 2.0 * np.pi * getattr(s.dt, attr) / period
+
+        X['{}_sin'.format(attr)] = np.sin(theta)
+        X['{}_cos'.format(attr)] = np.cos(theta)
 
     return X
 
