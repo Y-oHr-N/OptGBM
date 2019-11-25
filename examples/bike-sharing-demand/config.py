85c26bbaa7eb72f162466b7e62dad2fdc89a6a63
Y-oHr-N <kon.y.ohr.n@gmail.com>
2019-11-26 01:44:39 +0900

    Updata config.py

--- examples/bike-sharing-demand/config.py
+++ examples/bike-sharing-demand/config.py
@@ -1,13 +1,18 @@
 """Config."""
 
 import numpy as np
+import pandas as pd
 
 from optgbm.sklearn import OGBMRegressor
 from sklearn.compose import TransformedTargetRegressor
+from sklearn.model_selection import TimeSeriesSplit
 
 
-def transform_batch(X):
+def transform_batch(X: pd.DataFrame, train: bool = True) -> pd.DataFrame:
     """User-defined proprocessing."""
+    if train:
+        X = X.sort_index()
+
     s = X.index.to_series()
 
     X['unixtime'] = 1e-09 * s.astype('int64')
@@ -75,6 +80,7 @@ c.Recipe.transform_batch = transform_batch
 
 c.Recipe.model_instance = TransformedTargetRegressor(
     regressor=OGBMRegressor(
+        cv=TimeSeriesSplit(5),
         n_estimators=100_000,
         n_trials=100,
         random_state=0
