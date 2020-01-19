3e4ca439a3524e2b82ca55979d5ae3dc5f788526
Y-oHr-N <kon.y.ohr.n@gmail.com>
2020-01-19 14:52:06 +0900

    Use ModifiedCatBoostRegressor

--- examples/restaurant-revenue-prediction/config.py
+++ examples/restaurant-revenue-prediction/config.py
@@ -1,10 +1,22 @@
 """Config."""
 
-import numpy as np
 import pandas as pd
 
 from optgbm.sklearn import OGBMRegressor
+from pretools.estimators import Astype
+from pretools.estimators import CalendarFeatures
+from pretools.estimators import ClippedFeatures
+from pretools.estimators import CombinedFeatures
+from pretools.estimators import DropCollinearFeatures
+from pretools.estimators import ModifiedCatBoostRegressor
+from pretools.estimators import ModifiedColumnTransformer
+from pretools.estimators import ModifiedSelectFromModel
+from pretools.estimators import NUniqueThreshold
+from pretools.estimators import Profiler
+from pretools.estimators import RowStatistics
+from sklearn.compose import make_column_selector
 from sklearn.model_selection import TimeSeriesSplit
+from sklearn.pipeline import make_pipeline
 
 label_col = 'revenue'
 
@@ -14,51 +26,7 @@ def transform_batch(data: pd.DataFrame, train: bool = True) -> pd.DataFrame:
     if train:
         data = data.sort_values('Open Date')
 
-        # label = data[label_col]
-        # q25, q75 = np.quantile(label, [0.25, 0.75])
-        # iqr = q75 - q25
-        # is_inlier = (q25 - 1.5 * iqr <= label) & (label <= q75 + 1.5 * iqr)
-        # data = data[is_inlier]
-
-    s = data['Open Date']
-
-    data['{}_unixtime'.format(s.name)] = 1e-09 * s.astype('int64')
-
-    attrs = [
-        # 'year',
-        # 'weekofyear',
-        'dayofyear',
-        'quarter',
-        'month',
-        'day',
-        'weekday',
-        'hour',
-        'minute',
-        'second'
-    ]
-
-    for attr in attrs:
-        if attr == 'dayofyear':
-            period = np.where(s.dt.is_leap_year, 366.0, 365.0)
-        elif attr == 'quarter':
-            period = 4.0
-        elif attr == 'month':
-            period = 12.0
-        elif attr == 'day':
-            period = s.dt.daysinmonth
-        elif attr == 'weekday':
-            period = 7.0
-        elif attr == 'hour':
-            period = 24.0
-        elif attr in ['minute', 'second']:
-            period = 60.0
-
-        theta = 2.0 * np.pi * getattr(s.dt, attr) / period
-
-        data['{}_{}_sin'.format(s.name, attr)] = np.sin(theta)
-        data['{}_{}_cos'.format(s.name, attr)] = np.cos(theta)
-
-    return data.drop(columns='Open Date')
+    return data
 
 
 c = get_config()  # noqa
@@ -71,10 +39,43 @@ c.Recipe.read_params = {
 }
 c.Recipe.transform_batch = transform_batch
 
-c.Recipe.model_instance = OGBMRegressor(
-    cv=TimeSeriesSplit(5),
-    n_estimators=100_000,
-    n_trials=100,
-    random_state=0
+c.Recipe.model_instance = make_pipeline(
+    Profiler(label_col=label_col),
+    Astype(),
+    NUniqueThreshold(max_freq=None),
+    ModifiedColumnTransformer(
+        [
+            (
+                'categoricaltransformer',
+                NUniqueThreshold(),
+                make_column_selector(dtype_include='category')
+            ),
+            (
+                'numericaltransformer',
+                make_pipeline(
+                    DropCollinearFeatures(method='spearman', random_state=0),
+                    ClippedFeatures()
+                ),
+                make_column_selector(dtype_include='number')
+            ),
+            (
+                'timetransformer',
+                CalendarFeatures(dtype='float32', include_unixtime=True),
+                make_column_selector(dtype_include='datetime64')
+            ),
+            (
+                'othertransformer',
+                RowStatistics(dtype='float32'),
+                make_column_selector()
+            )
+        ]
+    ),
+    CombinedFeatures(include_data=True),
+    ModifiedSelectFromModel(
+        ModifiedCatBoostRegressor(random_state=0),
+        random_state=0,
+        # threshold=1e-06
+    ),
+    ModifiedCatBoostRegressor(random_state=0)
 )
 c.Recipe.model_path = 'examples/restaurant-revenue-prediction/model.pkl'
